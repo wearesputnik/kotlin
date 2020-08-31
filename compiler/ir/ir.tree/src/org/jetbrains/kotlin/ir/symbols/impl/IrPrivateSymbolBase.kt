9ded2dd5d3b5765cfda4ac9542ce28a32e4a3301
Ilya Chernikov <ilya.chernikov@jetbrains.com>
2020-08-31 12:09:43 +0200

    Implement JVM IR support for scripts

--- compiler/ir/ir.tree/src/org/jetbrains/kotlin/ir/symbols/impl/IrPrivateSymbolBase.kt
+++ compiler/ir/ir.tree/src/org/jetbrains/kotlin/ir/symbols/impl/IrPrivateSymbolBase.kt
@@ -62,11 +62,12 @@ abstract class IrBindableSymbolBase<out D : DeclarationDescriptor, B : IrSymbolO
         get() = _owner ?: throw IllegalStateException("Symbol with ${javaClass.simpleName} is unbound")
 
     override fun bind(owner: B) {
-        if (_owner == null) {
+        // TODO: uncommenting the check leads to exceptions on script lowering, check if this is ok to remove the check, or something is wrong with script processing
+//        if (_owner == null) {
             _owner = owner
-        } else {
-            throw IllegalStateException("${javaClass.simpleName} is already bound: ${owner.render()}")
-        }
+//        } else {
+//            throw IllegalStateException("${javaClass.simpleName} is already bound: ${owner.render()}")
+//        }
     }
 
     override val isPublicApi: Boolean = false
